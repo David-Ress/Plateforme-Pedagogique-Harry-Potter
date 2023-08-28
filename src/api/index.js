a196643c76196a42eb9c05c05ab7e275f8ab6814
David-Ress <david.ressicaud@hotmail.fr>
2023-08-28 16:16:16 +0200

    temporary fix until backend project fully done

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 
 // eslint-disable-next-line import/prefer-default-export
 export const axiosInstance = axios.create({
-  baseURL: 'harry-potter-back-production.up.railway.app',
+  baseURL: 'https://projet-07-harry-potter-back-production.up.railway.app/',
 });
